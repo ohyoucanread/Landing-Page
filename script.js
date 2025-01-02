9c5470aacbee72c25322f058e2ebd9eae5996534
Mehdi <mehdi.safi12@gmail.com>
2025-01-02 16:32:59 +0100

    functions, objects, DOM practices

--- script.js
+++ script.js
@@ -1,6 +1,41 @@
-let a = 6
+const product1 = {
+        name :'BASKETBALL ' ,
+        price : 300,
+        'delivery-time' : '3 days', 
+    };
+    const product2 = {
+        name :'Basketball' ,
+        price : 200,
+        'delivery-time' : '3 days', 
+    };
+
+console.log(product1,product2);
+console.log(product1.name  .repeat(2));
+
+
+comparePrice ();
+const results = isSameProduct(product1,product2);
+console.log(results);
+console.log()
+
+function comparePrice() {
+    if (product1.price > product2.price) {
+const {price} = product2;
+        console.log(price);
+    }
+    else {
+        const {price} = product1;
+        console.log(price);
+    }       
+}
+
+function isSameProduct(product1, product2){
     
-console.log(a);
+    if (product1.price === product2.price && product1.name === product2.name){
+        return true;
+    }
+        else {
+        return false;
+    }
 
-
-console.log("hello world");
+}
---
+++ script2.js
@@ -0,0 +1,5 @@
+const toDO = [];
+
+function addTheItem () {
+    
+}
